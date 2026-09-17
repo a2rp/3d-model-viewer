c6533c474e3a3acb2092c2d1f9f2f47e094c024c
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-09-17 12:01:14 +0530

    Configure Module Federation remote

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
 
 export default defineConfig({
-    plugins: [react()],
-
     base: "/3d-model-viewer/",
 
+    plugins: [
+        react(),
+
+        federation({
+            name: "threeDModelViewer",
+            filename: "remoteEntry.js",
+
+            exposes: {
+                "./App": "./src/remote/RemoteApp.jsx",
+            },
+
+            shared: {
+                react: {
+                    singleton: true,
+                },
+
+                "react-dom": {
+                    singleton: true,
+                },
+            },
+
+            dts: false,
+        }),
+    ],
+
     build: {
+        target: "esnext",
         sourcemap: false,
     },
 });
